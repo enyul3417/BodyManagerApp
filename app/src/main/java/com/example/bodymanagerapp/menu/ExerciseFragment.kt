ff4a3eed05e85c6cce15f05f3ab3aeb03551ba98
enyul3417 <dbfl973@naver.com>
2021-04-02 21:43:08 +0900

    making exercise xml

--- app/src/main/java/com/example/bodymanagerapp/menu/ExerciseFragment.kt
+++ app/src/main/java/com/example/bodymanagerapp/menu/ExerciseFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.bodymanagerapp.R
 
 class ExerciseFragment : Fragment() {
+    lateinit var timer_hour : EditText
+    lateinit var timer_minute : EditText
+    lateinit var timer_second : EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_exercise, container, false)
+        var view : View = inflater.inflate(R.layout.fragment_exercise, container, false)
+        timer_hour = view.findViewById(R.id.timer_hour)
+        timer_minute = view.findViewById(R.id.timer_minute)
+        timer_second = view.findViewById(R.id.timer_second)
+
+        return view
     }
 }
