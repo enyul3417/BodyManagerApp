6e1b44ed3f6cec4470e78407ac9fdcb88b72b1ee
Choi Yuri <49514531+enyul3417@users.noreply.github.com>
2021-05-03 01:43:35 +0900

    Creating Pet activity

---
+++ app/src/main/java/com/example/bodymanagerapp/menu/Pet/PetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bodymanagerapp.menu.Pet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bodymanagerapp.R
+
+class PetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet)
+    }
+}
